8feeb049fd969246f117b4183dbb84abb7bb7034
Gennadiy Naumovych <navmovn@gmail.com>
2024-04-27 15:32:03 +0300

    hw_16  done

---
+++ hw_16/script.js
@@ -0,0 +1,24 @@
+let name = "John",
+  surname = "Doe";
+let user_name = "Java";
+let user_surname;
+user_surname = "Script";
+/*let 123user_name;
+let user name = "Jack";
+let function = "Honda";
+let місто = "Texas";
+const my+const = 18;
+*/
+//одностроковий коментар
+/*багатостроковий 
+коментар*/
+/**документуючий коментарі, для опису поводження коду програми, зазвичай опис функцій */
+/*стилі написання імен змінних
+ camelCase (userName), UpperCamelCase (UserName), lower_case(user_name), UPPERCASE(USER_NAME)*/
+let studentName = prompt("ВВеди своє ім'я");
+alert("Привіт " + studentName);
+let currentYear = 2024;
+let birthYear = prompt("Введи свій рік народження");
+alert(`Твій вік ${currentYear - birthYear}`);
+let length = prompt("Введи довжину сторони квадрату");
+alert(`Периметр квадрату становить ${length * 4}`);
