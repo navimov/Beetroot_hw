ac53ba1935223908e110503604b957a8db481dd7
Gennadiy Naumovych <navmovn@gmail.com>
2024-05-08 21:50:25 +0300

    fix hw_19 minnorma

--- hw_19/minimum.js
+++ hw_19/minimum.js
@@ -17,7 +17,7 @@ function Car(
   this.drivers = drivers;
   /*Створи наступні методи для роботи з цим об'єктом:
 1.Метод, який виводить на екран інформацію про автомобіль.*/
-  this.showInfo = function showInfo() {
+  this.showInfo = function () {
     for (let key in this) {
       if (this.hasOwnProperty(key) && typeof this[key] !== "function") {
         console.log(key + ": " + this[key]);
@@ -25,15 +25,19 @@ function Car(
     }
   };
   //Метод додавання ім’я водія у список
-  this.addDriver = function addDriver(name) {
-    this.drivers += " Tyler";
+  this.addDriver = function (name) {
+    this.drivers += ` ${name}`;
   };
   //Метод перевірки водія на наявність його ім’я у списку
-  this.isAddedDriver = function isAddedDriver(name) {
-    return this.drivers.includes(name);
+  this.isAddedDriver = function (name) {
+    if (this.drivers.includes(name)) {
+      console.log(`Водій ${name} доданий до списку водіїв`);
+    } else {
+      console.log(`Водій ${name} відсутній у списку водіїв`);
+    }
   };
   /*Метод підрахунку необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. */
-  this.countTimeFuel = function countTimeFuel(distance) {
+  this.countTimeFuel = function (distance) {
     let drivingTime = distance / parseFloat(this.midSpeed);
     let needFuel = (distance / 100) * parseFloat(this.fuelAverage);
     let restTime = 0;
@@ -58,3 +62,7 @@ let car = new Car(
   "8.5L",
   "Nick john"
 );
+car.showInfo();
+car.addDriver("Crosser");
+car.isAddedDriver("tyler");
+car.countTimeFuel(1400);
--- hw_19/norma.js
+++ hw_19/norma.js
@@ -1,57 +1,60 @@
 //Створити об'єкт, що описує час (години, хвилини, секунди)
-let timeObj = {
-  hours: 10,
-  minutes: 45,
-  seconds: 54,
-};
-
-//Метод для виведення часу на екран.
-timeObj.showTime = function showTime() {
-  alert(
-    `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
-  );
-};
-timeObj.showTime();
-//Метод для зміни часу на передану кількість секунд.
-timeObj.changeSeconds = function changeSeconds(sec) {
-  this.seconds += sec;
-  if (this.seconds / 60 > 1) {
-    this.minutes += Math.floor(this.seconds / 60);
-    this.seconds = this.seconds % 60;
-    if (this.minutes / 60 > 1) {
-      this.minutes = this.minutes % 60;
-      this.hours += Math.floor(this.minutes / 60);
+function TimeObj(hours, minutes, seconds) {
+  this.hours = hours;
+  this.minutes = minutes;
+  this.seconds = seconds;
+  //Метод для виведення часу на екран.
+  this.showTime = function () {
+    alert(
+      `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
+    );
+  };
+  //Метод для зміни часу на передану кількість секунд.
+  this.changeSeconds = function (sec) {
+    this.seconds += sec;
+    if (this.seconds / 60 > 1) {
+      this.minutes += Math.floor(this.seconds / 60);
+      this.seconds = this.seconds % 60;
+      if (this.minutes / 60 > 1) {
+        this.minutes = this.minutes % 60;
+        this.hours += Math.floor(this.minutes / 60);
+      }
     }
-  }
-  alert(
-    `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
-  );
-};
-timeObj.changeSeconds(654);
-
-//Метод для зміни часу на передану кількість хвилин.
-timeObj.changeMinutes = function changeMinutes(min) {
-  this.minutes += min;
-  if (this.minutes / 60 > 1) {
-    this.hours += Math.floor(this.minutes / 60);
-    this.minutes = this.minutes % 60;
+    alert(
+      `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
+    );
+  };
+  //Метод для зміни часу на передану кількість хвилин.
+  this.changeMinutes = function (min) {
+    this.minutes += min;
+    if (this.minutes / 60 > 1) {
+      this.hours += Math.floor(this.minutes / 60);
+      this.minutes = this.minutes % 60;
+      if (this.hours / 24 > 1) {
+        this.hours = this.hours % 24;
+      }
+    }
+    alert(
+      `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
+    );
+  };
+  //Метод для зміни часу на передану кількість годин.
+  this.changeHours = function (hour) {
+    this.hours += hour;
     if (this.hours / 24 > 1) {
       this.hours = this.hours % 24;
     }
-  }
-  alert(
-    `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
-  );
-};
+    alert(
+      `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
+    );
+  };
+}
+let timeObj = new TimeObj(10, 45, 54);
+
+timeObj.showTime();
+
+timeObj.changeSeconds(654);
+
 timeObj.changeMinutes(540);
-//Метод для зміни часу на передану кількість годин.
-timeObj.changeHours = function changeHours(hour) {
-  this.hours += hour;
-  if (this.hours / 24 > 1) {
-    this.hours = this.hours % 24;
-  }
-  alert(
-    `Поточний час збережений в об'єкті ${this.hours}:${this.minutes}:${this.seconds}`
-  );
-};
+
 timeObj.changeHours(190);
