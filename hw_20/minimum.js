e09e469fff22ef00d301f71b8f6329be6f9b2ed5
Gennadiy Naumovych <navmovn@gmail.com>
2024-05-14 17:39:53 +0300

    fix hw_20 functions

--- hw_20/minimum.js
+++ hw_20/minimum.js
@@ -1,19 +1,19 @@
 //TASK1.Напиши всі можливі варіанти створення функцій.
-//function declaration
+function declaration
 function showMethod() {
   alert(
     `"function functionName(){do something}" This is function declaration method to create function`
   );
 }
-//function expression
+function expression
 let showMessage = function () {
   alert(
     `"let showMessage = function () {do something}" This is function expression method to create function`
   );
 };
-// arrow functions
+arrow functions
 let arrowFunc = () => 2 * 2;
-//  anonymous function
+ anonymous function
 (function (a, b) {
   return a + b;
 });
@@ -26,17 +26,17 @@ showMessage();
 showNumberParams("John", 15, "Brown");
 
 /*TASK3.Напиши функцію, яка приймає 2 числа і повертає :
--1, якщо перше число менше, ніж друге; 
-1 - якщо перше число більше, ніж друге; 
-0 - якщо числа рівні.*/
-function cnumberCmparison(num1, num2) {
-  alert(
-    `Результат порівняння чисел ${num1} і ${num2} рівний ${
-      num1 < num2 ? -1 : num1 > num2 ? 1 : 0
-    }`
-  );
-}
-cnumberCmparison(400, 200);
+// -1, якщо перше число менше, ніж друге; 
+// 1 - якщо перше число більше, ніж друге; 
+// 0 - якщо числа рівні.*/
+// // function cnumberCmparison(num1, num2) {
+// //   alert(
+// //     `Результат порівняння чисел ${num1} і ${num2} рівний ${
+// //       num1 < num2 ? -1 : num1 > num2 ? 1 : 0
+// //     }`
+// //   );
+// // }
+// // cnumberCmparison(400, 200);
 
 //TASK4.Напиши функцію, яка обчислює факторіал переданого їй числа.
 function getFactorial(num) {
@@ -62,16 +62,11 @@ concatNum(1, 5, 8);
 
 /*TASK6.Напиши функцію, яка приймає довжину і ширину прямокутника і обчислює його площу. Якщо в функцію передали 1 параметр, то вона обчислює площу квадрата.*/
 function getSquare(a, b) {
-  let resSquare =
     arguments[1] != undefined
-      ? arguments[0] * arguments[1]
-      : Math.pow(arguments[0], 2);
-  if (arguments[1] != undefined) {
-    alert(
-      `Площа прямокутника розміром ${arguments[0]} x ${arguments[0]} рівна ${resSquare}`
-    );
-  } else {
-    alert(`Площа квадрата зі стороною ${arguments[0]} рівна ${resSquare}`);
-  }
+      ?  alert(
+        `Площа прямокутника розміром ${arguments[0]} x ${arguments[1]} рівна ${a*b}`
+      )
+      : alert(`Площа квадрата зі стороною ${arguments[0]} рівна ${Math.pow(arguments[0], 2)}`);
+
 }
-getSquare(10);
+getSquare(10, 5);
